ccb6e38af37090fd1916a34b177417c9f1c8ff8a
Student <Student@ZAPRE-D00000150.DirectDelivery.com>
2017-10-11 11:11:17 +0200

    added d1 and d2

--- Cal2/src/cal2/Cal2.java
+++ Cal2/src/cal2/Cal2.java
@@ -7,12 +7,18 @@ public class Cal2
     
     public int add (String s1, String s2)
     {
-        return 0;
+        int d1 = Integer.parseInt(s1);
+        int d2 = Integer.parseInt(s2);
+        
+        return d1+d2;
     }
   
     
     public static void main(String[] args) 
     {
+   
+        
+ 
         
     }
     
