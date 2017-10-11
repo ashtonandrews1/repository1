52720d0545aa00e7b51a3ac9f68bef9236eedb48
Student <Student@ZAPRE-D00000150.DirectDelivery.com>
2017-10-11 11:04:23 +0200

    #Initial Project

--- Cal2/src/cal2/Cal2.java
+++ Cal2/src/cal2/Cal2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cal2;
 
-/**
- *
- * @author Student
- */
+
 public class Cal2 
 {
     
@@ -16,7 +9,7 @@ public class Cal2
     {
         return 0;
     }
-
+  
     
     public static void main(String[] args) 
     {
